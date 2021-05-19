d91156363a09775704ec73c2aaca0fde2467ba2a
Артём Бургарт <a.burgart02@gmail.com>
2021-05-19 19:49:34 +0500

    trapsandkey

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.gameObject.CompareTag("Key1"))
+        {
+            Debug.Log("1");
+            Destroy(gameObject);
+            Destroy(hitInfo.gameObject);
+        }
+    }
+}
