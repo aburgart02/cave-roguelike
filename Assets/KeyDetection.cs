fa04eb958736681202cfa9ccd59b5be0420646d7
contrust <2001raf@gmail.com>
2021-05-23 17:26:45 +0500

    DOOR OPENS BY PRESSING F KEY

--- Assets/KeyDetection.cs
+++ Assets/KeyDetection.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class KeyDetection : MonoBehaviour
 {
     public GameObject door;
-    public bool isClosing;
+    public bool opening;
+    public Transform openingPosition;
     public Transform closingPosition;
+    public bool canBeOpenedByKey;
     [SerializeField] public string keyTag = "GreenKey";
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(keyTag))
         {
-            isClosing = true;
+            opening = true;
+            canBeOpenedByKey = true;
             Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && canBeOpenedByKey && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Idi na hui");
+            opening = !opening;
         }
     }
 
     void Update()
     {
-        if (isClosing)
-            door.transform.position = Vector3.Lerp(door.transform.position, closingPosition.transform.position,  Time.deltaTime / 5);
+        if (opening)
+            door.transform.position = Vector3.Lerp(door.transform.position, openingPosition.transform.position,  Time.deltaTime / 4);
+        else
+            door.transform.position = Vector3.Lerp(door.transform.position, closingPosition.transform.position, Time.deltaTime / 4);
     }
 
 
