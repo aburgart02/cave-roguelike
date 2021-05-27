61ac9fbfd9a28c17a5a8c1d25b6fd00a220f81e1
Артём Бургарт <a.burgart02@gmail.com>
2021-05-27 21:53:53 +0500

    flamethrower

--- Assets/MineExplosion.cs
+++ Assets/MineExplosion.cs
@@ -7,17 +7,17 @@ public class MineExplosion : MonoBehaviour
     public int damage = 10;
     public GameObject ShootAnimation;
     public Transform explosionPosition;
-    [SerializeField] private AudioSource Explosion;
+    [SerializeField] private AudioClip Sound;
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         if (hitInfo.gameObject.CompareTag("Enemy") || hitInfo.gameObject.CompareTag("Player"))
         {
+            if (Sound != null)
+                AudioSource.PlayClipAtPoint(Sound, gameObject.transform.position);
             Instantiate(ShootAnimation, explosionPosition.position, Quaternion.identity);
             var enemyHealth = hitInfo.GetComponent<Health>();
             enemyHealth.TakeDamage(damage);
-            if (Explosion != null)
-                Explosion.Play();
         }
         Destroy(gameObject);
     }
